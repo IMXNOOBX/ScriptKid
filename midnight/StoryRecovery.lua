ce6ade12f8f877afb978eb50fad29bf0580cc15c
IMXNOOBX <paraelmobil403@gmail.com>
2022-07-22 20:57:15 +0200

    1.0.0

--- midnight/StoryRecovery.lua
+++ midnight/StoryRecovery.lua
@@ -1,14 +1,15 @@
 --[[
 **  github.com/IMXNOOBX            **
-**  Version: 1.0.0       		   **
+**  Version: 1.0.1       		   **
 **  github.com/IMXNOOBX/ScriptKid  **
 ]]
 
 function OnInit()
-	utils.notify('StoryRecovery', 'Unload the script to\nset the maximun Story Mode money!', gui_icon.settings, notify_type.warning)
+	if lobby.is_session_active() == true then utils.notify('StoryRecovery', 'Join "Story Mode" before doing anything!', gui_icon.settings, notify_type.error) else utils.notify('StoryRecovery', 'Unload the script to\nset the maximun Story Mode money!', gui_icon.settings, notify_type.warning) end
 end
 
 function OnDone()
+	if lobby.is_session_active() then return end
 	STATS.STAT_SET_INT('SP0_TOTAL_CASH', 2069696969, true) -- Michael
 	STATS.STAT_SET_INT('SP1_TOTAL_CASH', 2069696969, true) -- Franklin
 	STATS.STAT_SET_INT('SP2_TOTAL_CASH', 2069696969, true) -- Trevor
