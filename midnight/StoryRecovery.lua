305f423f86fb089c8be498d9bf03d1c3f7b7919b
IMXNOOBX <paraelmobil403@gmail.com>
2022-07-22 20:59:31 +0200

    1.0.1

--- midnight/StoryRecovery.lua
+++ midnight/StoryRecovery.lua
@@ -5,13 +5,13 @@
 ]]
 
 function OnInit()
-	if lobby.is_session_active() == true then utils.notify('StoryRecovery', 'Join "Story Mode" before doing anything!', gui_icon.settings, notify_type.error) else utils.notify('StoryRecovery', 'Unload the script to\nset the maximun Story Mode money!', gui_icon.settings, notify_type.warning) end
+	if lobby.is_session_active() then utils.notify('StoryRecovery', 'Join "Story Mode" before doing anything!', gui_icon.settings, notify_type.warning) else utils.notify('StoryRecovery', 'Unload the script to\nset the maximun Story Mode money!', gui_icon.settings, notify_type.warning) end
 end
 
 function OnDone()
 	if lobby.is_session_active() then return end
-	STATS.STAT_SET_INT('SP0_TOTAL_CASH', 2069696969, true) -- Michael
-	STATS.STAT_SET_INT('SP1_TOTAL_CASH', 2069696969, true) -- Franklin
-	STATS.STAT_SET_INT('SP2_TOTAL_CASH', 2069696969, true) -- Trevor
+	-- STATS.STAT_SET_INT('SP0_TOTAL_CASH', 2069696969, true) -- Michael
+	-- STATS.STAT_SET_INT('SP1_TOTAL_CASH', 2069696969, true) -- Franklin
+	-- STATS.STAT_SET_INT('SP2_TOTAL_CASH', 2069696969, true) -- Trevor
 	utils.notify('StoryRecovery', 'Money set!', gui_icon.settings, notify_type.default)
 end
