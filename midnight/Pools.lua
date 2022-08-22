3abda237cd4a9550982ee6d18af67685c3eec37a
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-08-22 17:25:53 +0200

    1.0.1 - pools

--- midnight/Pools.lua
+++ midnight/Pools.lua
@@ -34,7 +34,7 @@ function OnFrame()
 	-- local tz_x, tz_y = draw.get_text_size_x(wm_text), draw.get_text_size_y(wm_text) -- it sets at 0 when i open another script that reders something
 
 	-- -- Background
-	-- draw.set_rounding(rounding)
+	draw.set_rounding(rounding)
 	draw.set_color(0, r, g, b, a)
 	draw.rect_filled(mw + mx + config.x_add - 5, my + config.y_add - 5, mw + mx + config.x_add + 150, my + config.y_add + 80)
 
@@ -43,19 +43,6 @@ function OnFrame()
 	draw.text(mw + mx + config.x_add, my + 15 + config.y_add, tostring('Peds: '.. #pools.peds..' / 256')) -- Peds
 	draw.text(mw + mx + config.x_add, my + config.y_add, tostring('Vehicles: '.. #pools.vehicles..' / 300')) -- Vehicles
 	draw.text(mw + mx + config.x_add, my + 30 + config.y_add, tostring('Objects: '.. #pools.objects..' / 2300')) -- Objects
-	draw.text(mw + mx + config.x_add, my + 45 + config.y_add, tostring('Entities: '.. #pools.entities..' / 256')) -- Entities
+	draw.text(mw + mx + config.x_add, my + 45 + config.y_add, tostring('Entities: '.. #pools.entities..' / ?')) -- Entities
 	draw.text(mw + mx + config.x_add, my + 60 + config.y_add, tostring('Pickups: '.. #pools.pickups..' / 73')) -- Pickups
 end
-
-
-
--- function OnScriptEvent(ply, event, args)
---     msg = 'ply: '..tostring(ply)..' | Event: '..tostring(event)
---     for i = 1, #args do
---         msg = msg .. ' | Args['..tostring(i)..']: '..tostring(args[i])
---     end
---     print(msg)
--- end
-
-
--- [08.08.2022 19:39:04] ply: 0 | Event: -555356783 | Args[1]: 1 | Args[2]: 32 | Args[3]: -818787917 | Args[4]: 0 | Args[5]: 0 | Args[6]: 0 | Args[7]: 0 | Args[8]: 0 | Args[9]: 0 | Args[10]: 0 | Args[11]: 0 | Args[12]: 0 | Args[13]: 0 | Args[14]: 0 | Args[15]: 0 | Args[16]: 0 | Args[17]: 0 | Args[18]: 0 | Args[19]: 0 | Args[20]: 0 | Args[21]: 0 | Args[22]: 0 | Args[23]: 0 | Args[24]: 0 | Args[25]: 0 | Args[26]: 0 | Args[27]: 0 | Args[28]: 0 | Args[29]: 0 | Args[30]: 0 | Args[31]: 0 | Args[32]: 0 | Args[33]: 0 | Args[34]: 0 | Args[35]: 1
