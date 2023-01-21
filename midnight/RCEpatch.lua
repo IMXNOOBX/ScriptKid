de4267169e55b7ff1e421e323511893f08a5bdf8
жnoobж#6228 <69653071+IMXNOOBX@users.noreply.github.com>
2023-01-21 21:24:44 +0100

    Added new event, credits KARLEND#5838

--- midnight/RCEpatch.lua
+++ midnight/RCEpatch.lua
@@ -5,8 +5,14 @@
 **  credits: https://github.com/YimMenu/YimMenu/commit/f360d7f4366d9a591f1d1ee6c26e5bdf52fcdfb6 **
 ]]
 
+local events = {
+	[1354970087] = true,
+	[1279059857] = true,
+	[-343495611] = true
+}
+
 function OnScriptEvent(ply, event, args)
-	if event == 1279059857 or event == -343495611 then 
+	if events[event] then
 		utils.notify('RCE Exploit | Blocked', 'Blocked RCE event from: '..player.get_name(ply), gui_icon.warning, notify_type.success)
 		return false
 	end
